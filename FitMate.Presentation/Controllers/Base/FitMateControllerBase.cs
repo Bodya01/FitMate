854d39c1853262b8cf749bfd1e3eda12c2f5f778
Bohdan Kulish <bogdankulish5@gmail.com>
2023-11-27 23:57:37 +0200

    Removed redundant UserManager

--- FitMate.Presentation/Controllers/Base/FitMateControllerBase.cs
+++ FitMate.Presentation/Controllers/Base/FitMateControllerBase.cs
@@ -1,9 +1,7 @@
 ﻿using FitMate.Business.Interfaces;
 using FitMate.Core.UnitOfWork;
-using FitMate.Infrastructure.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FitMate.UI.Web.Controllers.Base
@@ -11,14 +9,12 @@ namespace FitMate.UI.Web.Controllers.Base
     [Authorize]
     public class FitMateControllerBase : Controller
     {
-        protected readonly UserManager<FitnessUser> _userManager;
         protected readonly IUserService _userService;
         protected readonly IMediator _mediator;
         protected readonly IUnitOfWork _unitOfWork;
 
-        public FitMateControllerBase(UserManager<FitnessUser> userManager, IMediator mediator, IUnitOfWork unitOfWork, IUserService userService)
+        public FitMateControllerBase(IMediator mediator, IUnitOfWork unitOfWork, IUserService userService)
         {
-            _userManager = userManager;
             _mediator = mediator;
             _unitOfWork = unitOfWork;
             _userService = userService;
