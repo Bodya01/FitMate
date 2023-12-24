033ae697ef48602b1dde1a9393f85ffb49ae0dd8
Bohdan Kulish <bogdankulish5@gmail.com>
2023-12-25 00:49:19 +0200

    Fixed issue with incorrect controller names

--- FitMate.Presentation/Helpers/UiNamingHelper.cs
+++ FitMate.Presentation/Helpers/UiNamingHelper.cs
@@ -4,7 +4,9 @@ namespace FitMate.Presentation.Helpers
 {
     internal static class UiNamingHelper
     {
-        public static string GetControllerName<T>() where T : FitMateControllerBase =>
-            nameof(T).Replace("Controller", string.Empty);
+        public static string GetControllerName<T>() where T : FitMateControllerBase
+        {
+            return typeof(T).Name.Replace("Controller", string.Empty);
+        }
     }
 }
