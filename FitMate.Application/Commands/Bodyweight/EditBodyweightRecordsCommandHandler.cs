e85f599a8389a1e80d87ca9e43da1f0ea112969f
Bohdan Kulish <bogdankulish5@gmail.com>
2024-01-03 22:44:26 +0200

    Refactored command for BW records edit

--- FitMate.Application/Commands/Bodyweight/EditBodyweightRecordsCommandHandler.cs
+++ FitMate.Application/Commands/Bodyweight/EditBodyweightRecordsCommandHandler.cs
@@ -20,21 +20,15 @@ namespace FitMate.Applcation.Commands.Bodyweight
         {
             var recordsToRemove = await _unitOfWork.BodyweightRecordRepository.Value.Get(e => e.UserId == command.UserId, s => s)
                 .ToListAsync(cancellationToken);
-            await _unitOfWork.BodyweightRecordRepository.Value.DeleteRangeAsync(recordsToRemove);
 
-            var records = new List<BodyweightRecord>();
-
-            for (var i = 0; i < command.RecordDates.Length; i++)
+            var records = command.RecordDates.Zip(command.RecordWeights, (date, weight) => new BodyweightRecord
             {
-                var newRecord = new BodyweightRecord()
-                {
-                    UserId = command.UserId,
-                    Date = command.RecordDates[i],
-                    Weight = command.RecordWeights[i]
-                };
-                records.Add(newRecord);
-            }
+                UserId = command.UserId,
+                Date = date,
+                Weight = weight
+            }).ToList();
 
+            await _unitOfWork.BodyweightRecordRepository.Value.DeleteRangeAsync(recordsToRemove, cancellationToken);
             await _unitOfWork.BodyweightRecordRepository.Value.CreateRangeAsync(records, cancellationToken);
             await _unitOfWork.SaveChangesAsync(cancellationToken);
         }
