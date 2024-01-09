7b6e9404aafae2195f13f3eb5f5c9ecbbe9f1c5c
Bohdan Kulish <bogdankulish5@gmail.com>
2024-01-10 01:22:06 +0200

    Fixed access modifier

--- FitMate.Presentation/ViewModels/Nutrition/NewFoodViewModel.cs
+++ FitMate.Presentation/ViewModels/Nutrition/NewFoodViewModel.cs
@@ -1,10 +1,9 @@
-﻿using FitMate.Infrastructure.Entities;
-using FitMate.Infrastucture.Dtos;
+﻿using FitMate.Infrastucture.Dtos;
 using System.Collections.Generic;
 
 namespace FitMate.Presentation.ViewModels.Nutrition
 {
-    public sealed class NewFoodViewModel
+    internal sealed class NewFoodViewModel
     {
         public IEnumerable<FoodDto> Foods { get; set; }
         public IEnumerable<FoodRecordDto> FoodRecords { get; set; }
