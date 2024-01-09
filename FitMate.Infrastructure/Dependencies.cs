14ae8c181ac75f7a56eb60119f05ba3d8bc78c65
Bohdan Kulish <bogdankulish5@gmail.com>
2024-01-10 01:24:36 +0200

    Refactored infrastructure dependency file

--- FitMate.Infrastructure/Dependencies.cs
+++ FitMate.Infrastructure/Dependencies.cs
@@ -6,64 +6,59 @@ namespace FitMate.Infrastructure;
 
 public static class Dependencies
 {
-    //TODO: Refactor
-    private const string CoreLayerPath = "../FitMate.Core/bin/Debug/net7.0/FitMate.Core.dll";
-    private const string BusinessLayerPath = "../FitMate.Business/bin/Debug/net7.0/FitMate.Business.dll";
+    private static string GetAssemblyPath(string assemblyName)
+    {
+        var executingAssemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        return Path.Combine(executingAssemblyPath, $"{assemblyName}.dll");
+    }
+
+    private static void InvokeDependencyMethod(string assemblyName, string typeName, string methodName, IServiceCollection services, IConfiguration configuration = null)
+    {
+        var assemblyPath = GetAssemblyPath(assemblyName);
+        var assembly = Assembly.LoadFrom(assemblyPath);
+        var type = assembly.GetType(typeName);
+        var methodInfo = type?.GetMethod(methodName);
+
+        if (methodInfo is not null)
+        {
+            var parameters = configuration is not null ? new object[] { services, configuration } : new object[] { services };
+            methodInfo.Invoke(null, parameters);
+        }
+    }
 
     public static IServiceCollection RegisterInfrastructure(this IServiceCollection services)
     {
         services.AddAutoMapper(typeof(Dependencies).Assembly);
-
         return services;
     }
 
     public static IServiceCollection RegisterContext(this IServiceCollection services, IConfiguration configuration)
     {
-        var assembly = Assembly.LoadFrom(CoreLayerPath);
-        var type = assembly.GetType("FitMate.Core.Dependencies");
-        var methodInfo = type?.GetMethod("RegisterContext");
-        methodInfo?.Invoke(null, new object[] { services, configuration });
-
+        InvokeDependencyMethod("FitMate.Core", "FitMate.Core.Dependencies", "RegisterContext", services, configuration);
         return services;
     }
 
     public static IServiceCollection MigrateDatabase(this IServiceCollection services)
     {
-        var assembly = Assembly.LoadFrom(CoreLayerPath);
-        var type = assembly.GetType("FitMate.Core.Dependencies");
-        var methodInfo = type?.GetMethod("MigrateDatabase");
-        methodInfo?.Invoke(null, new object[] { services });
-
+        InvokeDependencyMethod("FitMate.Core", "FitMate.Core.Dependencies", "MigrateDatabase", services);
         return services;
     }
 
     public static IServiceCollection RegisterIdentity(this IServiceCollection services)
     {
-        var assembly = Assembly.LoadFrom(CoreLayerPath);
-        var type = assembly.GetType("FitMate.Core.Dependencies");
-        var methodInfo = type?.GetMethod("RegisterIdentity");
-        methodInfo?.Invoke(null, new object[] { services });
-
+        InvokeDependencyMethod("FitMate.Core", "FitMate.Core.Dependencies", "RegisterIdentity", services);
         return services;
     }
 
     public static IServiceCollection RegisterRepositories(this IServiceCollection services)
     {
-        var assembly = Assembly.LoadFrom(CoreLayerPath);
-        var type = assembly.GetType("FitMate.Core.Dependencies");
-        var methodInfo = type?.GetMethod("RegisterRepositories");
-        methodInfo?.Invoke(null, new object[] { services });
-
+        InvokeDependencyMethod("FitMate.Core", "FitMate.Core.Dependencies", "RegisterRepositories", services);
         return services;
     }
 
     public static IServiceCollection RegisterServices(this IServiceCollection services)
     {
-        var assembly = Assembly.LoadFrom(BusinessLayerPath);
-        var type = assembly.GetType("FitMate.Business.Dependencies");
-        var methodInfo = type?.GetMethod("RegisterServices");
-        methodInfo?.Invoke(null, new object[] { services });
-
+        InvokeDependencyMethod("FitMate.Business", "FitMate.Business.Dependencies", "RegisterServices", services);
         return services;
     }
 }
