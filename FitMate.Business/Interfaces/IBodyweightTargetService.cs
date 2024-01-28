0e14ac4d0d4cbf44ebe2e53de33da6887ecd915d
Bohdan Kulish <bogdankulish5@gmail.com>
2024-01-28 11:36:46 +0200

    Implemented method to get current user target

--- FitMate.Business/Interfaces/IBodyweightTargetService.cs
+++ FitMate.Business/Interfaces/IBodyweightTargetService.cs
@@ -1,9 +1,11 @@
 ﻿using FitMate.Infrastructure.Models.BodyweightTarget;
+using FitMate.Infrastucture.Dtos.Base;
 
 namespace FitMate.Business.Interfaces
 {
     public interface IBodyweightTargetService
     {
+        Task<BodyweightTargetDto> GetCurrentTargetAsync(string userId, CancellationToken cancellationToken);
         Task UpdateTargetAsync(UpdateBodyweightTargetModel model, CancellationToken cancellationToken = default);
     }
 }
--- FitMate.Business/Services/BodyweightTargetService.cs
+++ FitMate.Business/Services/BodyweightTargetService.cs
@@ -3,7 +3,9 @@ using FitMate.Business.Interfaces;
 using FitMate.Business.Services.Base;
 using FitMate.Core.UnitOfWork;
 using FitMate.Infrastructure.Entities;
+using FitMate.Infrastructure.Exceptions;
 using FitMate.Infrastructure.Models.BodyweightTarget;
+using FitMate.Infrastucture.Dtos.Base;
 using Microsoft.EntityFrameworkCore;
 
 namespace FitMate.Business.Services
@@ -11,6 +13,17 @@ namespace FitMate.Business.Services
     internal sealed class BodyweightTargetService : ServiceBase, IBodyweightTargetService
     {
         public BodyweightTargetService(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper) { }
+
+        public async Task<BodyweightTargetDto> GetCurrentTargetAsync(string userId, CancellationToken cancellationToken)
+        {
+            var entity = await _unitOfWork.BodyweightTargetRepository.Value.Get(e => e.UserId == userId, s => s)
+                .OrderByDescending(e => e.TargetDate)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (entity is null) throw new EntityNotFoundException($"User with id {userId} does not have any bodyweight targets");
+
+            return _mapper.Map<BodyweightTargetDto>(entity);
+        }
 
         public async Task UpdateTargetAsync(UpdateBodyweightTargetModel model, CancellationToken cancellationToken = default)
         {
