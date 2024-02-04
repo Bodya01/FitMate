0bb58191637accc733b3a22dc6fffbcd7f2cb8c5
Bohdan Kulish <bogdankulish5@gmail.com>
2024-02-04 22:06:33 +0200

    Removed redundant fields

--- FitMate.Infrastructure/Entities/Interfaces/IGoal.cs
+++ FitMate.Infrastructure/Entities/Interfaces/IGoal.cs
@@ -5,8 +5,4 @@ internal interface IGoal : IUserOwnedEntity
     public Guid Id { get; set; }
 
     public string Activity { get; set; }
-
-    public string UserId { get; set; }
-
-    public FitnessUser User { get; set; }
 }
--- FitMate.Infrastructure/Entities/Interfaces/IGoalProgress.cs
+++ FitMate.Infrastructure/Entities/Interfaces/IGoalProgress.cs
@@ -5,8 +5,4 @@ public interface IGoalProgress : IUserOwnedEntity
     public Guid Id { get; set; }
 
     public DateTime Date { get; set; }
-
-    public string UserId { get; set; }
-
-    public FitnessUser User { get; set; }
 }
